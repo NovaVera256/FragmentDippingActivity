c79ce7a6f9bc5120f96c6764025f849ec1c91f1c
NovaVera256 <novastreakgaming@outlook.com>
2025-10-07 12:08:24 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/fragmentdippingactivity/ColorFragment.kt
@@ -0,0 +1,26 @@
+package com.example.fragmentdippingactivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.graphics.toColorInt
+
+
+class ColorFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color, container, false).apply{
+            setOnClickListener {
+                setBackgroundColor(
+                    arrayOf("Blue", "Maroon", "Magenta", "Red", "Green").random().toColorInt()
+                )
+            }
+        }
+    }
+}
